57b4f14a0aa17f5763ab36600c311e69af01e851
Никола Веселков <keha39@yandex.ru>
2023-12-14 23:16:27 +0300

    demo

---
+++ TemplateCCNew8/LessonOne/LessonOne.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TemplateCCNew8.LessonOne
+{
+    public class LessonOne : Control
+    {
+        static LessonOne()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof( LessonOne ),
+                new FrameworkPropertyMetadata( typeof( LessonOne ) ) );
+        }
+    }
+}
---
+++ TemplateCCNew8/LessonTwo/LessonTwo.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TemplateCCNew8.LessonTwo;
+
+public class LessonTwo : Control
+{
+    static LessonTwo()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(
+            typeof( LessonTwo ),
+            new FrameworkPropertyMetadata( typeof( LessonTwo ) ) );
+    }
+}
